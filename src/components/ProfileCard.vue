deb4a06dcb0073aa408672cc8b0051237b64a61b
Tom Lee <thomas.lee@uconn.edu>
2020-04-29 13:03:55 -0400

    Start profile section

---
+++ src/components/ProfileCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-hover v-slot:default="{ hover }">
+    <v-card tile class="d-flex" :class="{ 'on-hover': hover }" v-on:click="moveDown">
+      <v-img
+        :src="require('../assets/mariacolonsanchez.jpg')"
+        contain
+        aspect-ratio="1"
+        height="200"
+        class="align-end"
+      >
+        <template v-slot:placeholder>
+          <v-row class="fill-height ma-0" align="center" justify="space-between">
+            <!-- <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular> -->
+          </v-row>
+        </template>
+      </v-img>
+      <v-card-text v-show="hover" class="font-weight-bold" v-on:click="moveDown">Maria Colon Sanchez</v-card-text>
+    </v-card>
+  </v-hover>
+</template>
+
+<script>
+
+export default {
+  methods: {
+    moveDown: function() {
+      this.$emit('profileCardClicked')
+    // console.log(this.$parent.$parent.$parent.$options.name)
+    // console.log(this.$root.$refs);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.v-card:not(.on-hover) {
+  opacity: 0.6;
+}
+</style>
--- src/components/Profiles.vue
+++ src/components/Profiles.vue
@@ -1,18 +1,47 @@
 <template>
-  <div class="section">
-    <h1>Profiles</h1>
-    <div v-for="slide in slides" v-bind:key="slide" class="slide">
-      <h1>{{slide}}</h1>
+  <div>
+    <div class="section">
+      <h1>Profiles</h1>
+      <v-row>
+        <v-col cols="12" sm="6" offset-sm="3">
+          <v-container fluid>
+            <!-- <h3>{{slide}}</h3> -->
+            <v-row>
+              <v-col v-for="n in 6" :key="n" class="d-flex child-flex" cols="4">
+                <ProfileCard></ProfileCard>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-col>
+      </v-row>
+    </div>
+    <div class="section" v-for="prof in profiles" :key="prof">
+      <v-row >
+        <v-col>
+          {{prof}}
+          <v-img :src="require('../assets/mariacolonsanchez.jpg')" max-height="500" contain></v-img>
+        </v-col>
+      </v-row>
     </div>
   </div>
 </template>
 
 <script>
+import ProfileCard from "@/components/ProfileCard";
 export default {
   name: "Profiles",
+  components: {
+    ProfileCard
+  },
   data() {
     return {
-      slides: ["Tenant Activists Profiles", "Historic Firsts"]
+      slides: ["Tenant Activists Profiles", "Historic Firsts"],
+      profiles: [
+        "Milagros Ortiz",
+        "Teri Morrison",
+        "Joshua Serrano",
+        "Ashley Johnson"
+      ]
     };
   }
 };
