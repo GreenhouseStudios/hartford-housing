e573207c8678304f3a017b14d356333309e26145
Mackenzie Fox <mackenzie.fox@uconn.edu>
2020-04-30 16:14:05 -0400

    Fill Text Added
    Settlements and Housing sections have cards with text (those with lorem ipsum filler are marked in the comments).

--- src/components/Housing.vue
+++ src/components/Housing.vue
@@ -1,88 +1,153 @@
 <template>
   <div class="section">
     <h1>Housing</h1>
-       <!-- <v-simple-table dark>
-        <template v-slot:default>
-          <thead>
-            <tr>
-              <th class="text-left">Name</th>
-              <th class="text-left">Calories</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="item in desserts" :key="item.name">
-              <td>{{ item.name }}</td>
-              <td>{{ item.calories }}</td>
-            </tr>
-          </tbody>
-        </template>
-      </v-simple-table> -->
-    <div v-for="slide in slides" v-bind:key="slide" class="slide">
-      <h1>{{slide}}</h1>
-   
+    <!-- <div v-for="slide in slides" v-bind:key="slide" class="slide">
+      <h1>{{slide}}</h1> -->
+
+    <div v-for="item in slides" v-bind:key="item.heading" class="slide">
+      <!-- <h1>{{item.heading}}</h1> -->
+      <Card v-bind:heading="item.heading" v-bind:body="item.body">
+
+      </Card>
     </div>
   </div>
 </template>
 
 <script>
+import Card from '@/components/Card'
 export default {
   name: "Housing",
+  components:{
+    Card
+  },
   data() {
     return {
       slides: [
-        "Post WWII Housing Crisis",
-        "Public Housing",
-        "Charter House Terrace",
-        "Bellevue Square",
-        "Redlining",
-        "Home Ownership",
-        "Slumlords Now, Slumlords Then",
-        "Housing Code Violations",
-        "The No More Slumlords Campaign"
-      ],
-      desserts: [
         {
-          name: "Frozen Yogurt",
-          calories: 159
+          heading: "Post WWII Housing Crisis",
+          body:`World War II ushered in a period of intense demographic shifts that intersected with
+a housing crisis that was already half a century in the making. The nation’s citizens
+deployed overseas to various theaters of war, leaving a gaping demand for industrial,
+manufacturing, and agricultural labor. The United States turned to its neighbors in the
+Caribbean to fill these temporary shortages and the boom economy spurred many
+people to move to major urban centers. Everyone was on the move—from white
+communities of various ethnic backgrounds to African Americans, West Indians, and
+Puerto Ricans. Following the war, the US owed a debt to veterans and the auxiliary
+
+civilian population that had contributed to the war effort. The war-time and post-
+war housing crisis led to policy interventions focused on slum clearance and the
+
+construction of public housing. The New England area needed 400,00 new homes
+with Connecticut facing a shortage of 80,000 new housing units.`
+        },
+        { // lorem ipsum
+          heading:"Public Housing",
+          body:`Relief strengthening infrastructure collective impact leverage then. Strengthening 
+          infrastructure thought leader scale and impact, commitment, collective impact social innovation 
+          mobilize move the needle vibrant. The resistance bandwidth thought provoking the resistance 
+          change-makers shared vocabulary. Transparent save the world activate external partners equal opportunity. 
+          Social entrepreneurship design thinking, targeted, milestones challenges and opportunities white paper. 
+          Indicators compassion blended value disrupt relief. Mobilize parse sustainable inspirational circular 
+          empathetic, shared unit of analysis blended value collective impact. Accessibility transparent venture 
+          philanthropy; boots on the ground resist shared unit of analysis shine support peaceful. Co-creation 
+          systems thinking humanitarian social entrepreneur systems thinking preliminary thinking. Ideate; philanthropy 
+          vibrant social return on investment, social entrepreneurship initiative capacity building white paper. `
         },
         {
-          name: "Ice cream sandwich",
-          calories: 237
+          heading:"Charter Oak Terrace",
+          body:`With 1000 units Charter Oak Terrace held the promise of addressing
+the severe shortage of affordable housing in Hartford. The unit located at Flatbush and Newfield Avenues, and Dart and Chandler Streets, had
+twice the occupancy of Bellevue Square and opened in 1941. Whereas Bellevue Square sat on 12.5 acres, Charter Oak Terrace covered 124 acres.
+The design, like other public housing units, encompassed recreational, educational, medical, and religious services. The Hartford Good Will Boys’
+Club hosted recreational activities and the Christian Activities Council of Hartford conducted Sunday School and Church Services. Children
+attended the local community school at Mary Hooker School, which like the housing complex, was new.
+This planned community represented both an important policy intervention and a particular vision of urban planning. Children would
+have access to education, healthcare and religious services, and would be reared in a wholesome environment that promoted family values.`
         },
         {
-          name: "Eclair",
-          calories: 262
+          heading:"Bellevue Square",
+          body:`For the African American families who turned to public housing to address the severe shortage of suitable
+accommodations in Hartford, the possibility of a better standard of living was a real promise. Not only was it
+difficult to find suitable places to live, even with high rents many accommodations had no heat and hot water;
+ice had to be bought from the trucks on the street and clothes had to be ironed with a heavy metal iron.
+Florence Kiser Price Wollaston, who grew up at Bellevue Square, recalls how her parents moved around in the
+1940s trying to find the right apartment in Hartford. The family moved up and down Canton Street in the 1940s,
+which would constantly flood at its intersection with Bellevue Street. Her father helped to build the dike to
+prevent these floods. Each move the Wollaston family made was in search of a better place to live.`
         },
         {
-          name: "Cupcake",
-          calories: 305
+          heading:"Redlining",
+          body:`The Great Depression devastated families across the United States, unleashed a wave of
+foreclosures, and spawned a major crisis in the housing sector. The federal government created a
+new infrastructure to facilitate home ownership by changing the way people financed and insured
+their homes. The National Housing Act of 1934 established the Federal Housing Administration
+(FHA) and the Federal Savings and Loan Insurance Corporation. This framework, which gave
+us our current system of 30-year mortgages, laid the foundation for racial discrimination in the
+housing market. Using the language of risk management, the FHA supported the classification of
+neighborhoods according to their demographic profile. The process known as redlining coded
+maps according to the desirability and viability of loans in particular communities. Many of
+Hartford’s ethnic and racial minorities lived in neighborhood branded Grades C or D, the two
+lowest categories, making them ineligible for federally-backed loans. Even veterans in these
+neighborhoods faced difficulties accessing these loans.`
         },
         {
-          name: "Gingerbread",
-          calories: 356
+          // lorem ipsum
+          heading:"Home Ownership",
+          body:`Social entrepreneur circular then inclusive the resistance progress replicable. 
+          Incubator then boots on the ground innovate cultivate. Her body her rights, correlation think 
+          tank correlation empower entrepreneur scale and impact. Strengthening infrastructure issue 
+          outcomes replicable agile; correlation, segmentation accessibility correlation. When when, synergy 
+          accessibility targeted; collaborate. Her body her rights innovation communities inclusive incubator 
+          improve the world ideate thought leadership. Do-gooder scale and impact shared vocabulary co-create 
+          indicators improve the world. Grit outcomes; challenges and opportunities, innovate move the needle 
+          commitment. Humanitarian a replicable uplift energize compelling changemaker. Shared unit of analysis 
+          synergy collaborate mass incarceration equal opportunity global collaborative consumption.`
         },
         {
-          name: "Jelly bean",
-          calories: 375
+          heading:"Slumlords Now, Slumlords Then",
+          body:`Even middle
+income families facing poor housing choices can have their health and job prospects ruined when they have
+to spend emotional and financial capital addressing horrendous living conditions. Hartford again made
+headlines when the United States Department of Housing and Urban Development (HUD) terminated the
+contract of New York’s infamous landlord, Emmanuel Ku. After years of inadequate maintenance and
+deplorable living conditions that posed a risk to his tenants’ health and safety, tenants and community
+activists finally won their bid to oust Ku. An organized group of tenants demonstrated that ordinary people
+could win against a wealthy, unscrupulous landlord. HUD’s regime of inspections continues to fail many
+of the clients who rely on the agency to certify that housing units are suitable for occupancy.`
         },
         {
-          name: "Lollipop",
-          calories: 392
+          heading: "Housing Code Violation",
+          body:`MICE. MOLD. BED BUGS. POOR VENTILATION. LACK OF HEAT. 
+          BROKEN WINDOWS. MISSING SCREENS.
+These violations are legion among properties owned by slumlords, yet often fall through the cracks
+based on inspection protocols that emphasize the exterior of the building. In many instances,
+misperceptions about the character of the tenants may lead the public to make value judgments about
+what people living in public housing deserve. The No More Slumlords movement has worked to change
+these perceptions. New horizons remain in the struggle.`
         },
         {
-          name: "Honeycomb",
-          calories: 408
-        },
-        {
-          name: "Donut",
-          calories: 452
-        },
-        {
-          name: "KitKat",
-          calories: 518
+          heading: "The No More Slumlords Campaign",
+          body:`The No More Slumlords campaign has become a beacon for other residents interested in addressing
+substandard and unsafe conditions in their housing units. Since that pivotal victory, resident-leaders like
+Teri Morrison, Milagros Ortiz, and Joshua Serrano have attended dozens of tenant meetings rallies, public
+hearings, panel discussions, and coalition meetings to remind the public that the work has only just
+begun. Overhauling Hartford’s outdated, ineffectual municipal housing code was one new prong in the
+movement; navigating the challenge of relocating to another unit proved to be another. Some families,
+like Milagros Ortiz’s, have had to move more than once, while others have left the state because of the
+limited resettlement options. Housing conditions have proven no better in some instances and many
+tenants have expressed distress about how dispersal has atomized the sense of community. Limited
+financial help, unrealistic deadlines, and feckless relocation specialist have wrought further emotional
+and financial havoc on tenants who have had to attend to their families’ needs while propelling a major
+social movement forward.`
         }
       ]
     };
   }
 };
 </script>
+
+<style scoped>
+.slide{
+  /* background-color: rgb(81, 159, 200); */
+}
+</style>
--- src/components/Settlement.vue
+++ src/components/Settlement.vue
@@ -34,24 +34,75 @@ Hartford became a Puerto Rican majority city.
 a major arena for civil rights and human rights advocacy.`
         },
         {
-        heading: "Georgia,Puerto Rico, Jamaica",
-        body: ""
+        heading: "Georgia, Puerto Rico, Jamaica",
+        body: `Correlation technology; scalable we must stand up collective impact granular. We must stand up and; movements gender 
+        rights state of play think tank mobilize. Social entrepreneur do-gooder optimism data natural resources humanitarian. 
+        Paradigm society empower justice sustainable, capacity building program areas. Co-create society strategy inspire human-centered. 
+        To radical data human-centered leverage inspire. Parse empathetic state of play her body her rights effective social entrepreneur 
+        effective relief. Optimism, ideate to impact then grit. Program area optimism radical shine inclusion, her body her rights scale 
+        and impact commitment social entrepreneur. To energize to expose the truth then revolutionary her body her rights. Because shared 
+        unit of analysis green space invest entrepreneur do-gooder accessibility. A, game-changer segmentation parse improve the world 
+        blended value NGO ideate optimism. Challenges and opportunities strategize empower uplift, strategize impact investing change-makers. 
+        Relief; emerging or, movements, youth academic thought leader. Blended value relief; support; black lives matter technology empathetic.`
         },
         {
         heading: "African American Settlement",
-        body:""
+        body:`“Disfranchisement, peonage, the abuses of the chain-gang, the leasing of prisoners as laborers, the easy surrender of
+prisoners by sheriffs to the leaders of mobs, the lynchings without trial, the Jim Crow cars where the ordinary decencies
+of life are not respected:” These were among the litany of offenses that The Crisis, published by the National Association
+for the Advancement of Colored People (NAACP) identified in its November 1923 issue as some of the push factors
+propelling African Americans to leave to the South. By 1970, six million African Americans had embarked on the Great
+Migration for new lives in Northeastern, Midwestern, and Western States.
+Besides the trans-Atlantic slave trade, this was the largest mass movement of peoples of African descent. Like the
+centuries of involuntary migration that preceded it, the Great Migration would reshape American cities. By 1930, forty
+percent of the African American population in Hartford identified their original home as Georgia. Another 15 percent
+originated from the Carolinas and Virginia. Americus, Plains, Ellaville, and other Georgian locales kindled a long pattern
+of chain migration to Hartford that would register profoundly in the residential makeup of the North End of Hartford and
+especially in the public housing projects of the 1940s.`
         },
         {
         heading: "West Indian Settlement",
-        body:""
+        body:`The 1950 census identified about 245
+West Indians in Connecticut, many of
+them men who had come to the United
+States in the 1940s to contribute to the
+war effort.
+
+By 2010, West Indians surpassed all other ethnic groups to become the largest population of foreign-born immigrants in Connecticut. This
+demographic trend was seven decades in the making, with the original kernel of this population arriving as guest workers in the Hartford
+region in the 1940s through bilateral labor agreements between British West Indian colonies and the United States. The vast majority were
+Jamaicans, but there were also Trinidadians and Barbadians as well.
+By the time these war-time labor opportunities appeared, a significant West Indian population had settled in New York, a population that
+had already reached 54,000 by the 1930s. In Connecticut, the draw was primarily the tobacco industry which benefited from the labor
+distributed through the industrial and agricultural concessions made for war-time. The diaspora of West Indian laborers established a post-
+emancipation labor tradition that created new spaces to recast and renegotiate the labor of black men and later, women.`
         },
         {
         heading: "Puerto Rican Settlement",
-        body:""
+        body:`Between 1950 and 1960,
+470,000 people, or approximately
+one-fifth of Puerto Rico’s population,
+relocated to the United States. New York attracted the vast majority of these sojourners who settled in manufacturing, service, maintenance, and
+domestic jobs. Like other ethnic groups before and in concert with them, Puerto Ricans sought better economic
+opportunities in their cities of first entry before moving out to nearby cities like Philadelphia, Chicago, Bridgeport and
+Hartford, to join family or pursue other opportunities.
+The Jones Act of 1917 granted Puerto Ricans U.S. citizenship, making relocation to the mainland far easier in principle than
+other groups impacted by the restrictive immigration policies that closed US borders for the next four decades. Yet farm
+labor programs offering employment also played an important role in facilitating the initial transportation to the United
+States. These programs recruited 421,238 Puerto Ricans workers between 1948 and 1990, the second largest mobilization
+of seasonal labors after the Mexican Bracero program.`
         },
         {
         heading: "Headlines",
-        body: ""
+        body: `Slumlords. Urban blight. Black mold. Rodent
+infestations and numerous other housing codeviolations. Recent Hartford Courant headlines
+continue to document the enduring legacies ofsegregated housing and poverty in Hartford.
+“Hartford has the Highest Rate of Urban Dwellers Living in Economically Troubled Neighborhoods,”
+a June 2018 Courant headline reads. These headlines are disturbingly timeless for many of the ethnic and
+racial minorities who call the city their home. They could have described any decade between the 1930s
+and 2010s as poverty limited housing options and poor housing created or exacerbated poverty. Even middle
+income families facing poor housing choices can have their health and job prospects ruined when they have
+to spend emotional and financial capital addressing horrendous living conditions.`
         }
       ]
     };
