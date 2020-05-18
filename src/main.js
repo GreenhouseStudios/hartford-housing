efe2e681ce9190e520207101baee528bff0dd5e0
Tom Lee <thomas.lee@uconn.edu>
2020-05-18 16:02:33 -0400

    scrollmagic

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import App from './App.vue'
 import VueFullPage from 'vue-fullpage.js'
 import vuetify from './plugins/vuetify';
 import router from './router'
+import VueScrollmagic from 'vue-scrollmagic'
 
 
 Vue.config.productionTip = false
 Vue.use(VueFullPage);
+Vue.use(VueScrollmagic)
 
 new Vue({
   vuetify,
