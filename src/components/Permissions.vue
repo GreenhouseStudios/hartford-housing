dac97d8eced82e5e94ef22608540ccd3db45313d
Tom Lee <thomas.lee@uconn.edu>
2020-06-25 09:21:54 -0400

    permissions starter text

--- src/components/Permissions.vue
+++ src/components/Permissions.vue
@@ -1,13 +1,12 @@
 <template>
   <div>
-    <h1 class="pa-5">Permissions</h1>
-    <v-row>
-      <v-col></v-col>
-      <v-col>
-        
-      </v-col>
-      <v-col></v-col>
-    </v-row>
+    <h1 class="display-3 pa-5">Permissions</h1>
+    <v-container>
+      <v-row>
+        <h1 class="dispay-3">Rights and Permissions:</h1>
+        <p>All items on the Hartford Bound website are used with the permission of their original sources and repositories; rights and permissions to use and distribute those items are held with the original copyright owners, unless otherwise specified.</p>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
