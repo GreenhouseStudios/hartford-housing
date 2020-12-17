8f60ced94043b235e2b8a19b96cdb440e67fea41
Thomas Lee <thomas.lee@uconn.edu>
2020-12-17 12:27:50 -0500

    funders links

--- src/components/Funders.vue
+++ src/components/Funders.vue
@@ -5,7 +5,7 @@
       <v-col></v-col>
       <v-col>
         <v-row v-for="(funder, index) in funders" v-bind:key="index" class="pa-5">
-          <a class="font-weight-bold">{{funder}}</a>
+          <a class="font-weight-bold" :href="funder.link">{{funder.name}}</a>
         </v-row>
       </v-col>
       <v-col></v-col>
@@ -19,14 +19,14 @@ export default {
   data() {
     return {
       funders: [
-        "AFRICANA STUDIES INSTITUTE (ASI)",
-        "COLLEGE OF LIBERAL ARTS AND SCIENCES (CLAS), OFFICE OF THE DEAN",
-        "EL INSTITUTO: INSTITUTE OF LATINA/O, CARIBBEAN, AND LATIN AMERICAN STUDIES (ELIN)",
-        "HARTFORD STAGE HISTORY DEPARTMENT",
-        "HUMAN RIGHTS INSTITUTE",
-        "OFFICE OF DIVERSITY AND INCLUSION (ODI)",
-        "PUERTO RICAN AND LATIN AMERICAN CULTURAL CENTER",
-        "THE THOMAS J. DODD RESEARCH CENTER URBAN AND COMMUNITY STUDIES"
+        {name: "AFRICANA STUDIES INSTITUTE (ASI)", link: "https://africana.uconn.edu/"},
+        {name: "COLLEGE OF LIBERAL ARTS AND SCIENCES (CLAS), OFFICE OF THE DEAN",link: "https://clas.uconn.edu/"},
+        {name: "EL INSTITUTO: INSTITUTE OF LATINA/O, CARIBBEAN, AND LATIN AMERICAN STUDIES (ELIN)",link: "https://elin.uconn.edu/"},
+        {name: "HARTFORD STAGE HISTORY DEPARTMENT",link: "https://www.hartfordstage.org/"},
+        {name: "HUMAN RIGHTS INSTITUTE",link: "https://humanrights.uconn.edu/"},
+        {name: "OFFICE OF DIVERSITY AND INCLUSION (ODI)",link: "https://diversity.uconn.edu/"},
+        {name: "PUERTO RICAN AND LATIN AMERICAN CULTURAL CENTER",link: "https://latinx.uconn.edu/"},
+        {name: "THE THOMAS J. DODD RESEARCH CENTER URBAN AND COMMUNITY STUDIES", link: "https://dodd.uconn.edu/"},
       ]
     };
   }
