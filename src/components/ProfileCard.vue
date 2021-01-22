68c4202f2d108f821800bdef19e3366579f8f73c
Mackenzie Fox <mackenzie.fox@uconn.edu>
2021-01-22 12:17:22 -0500

    Profile Card Title Anchored to Bottom
    Moved the title card down to the bottom, appears on hover

--- src/components/ProfileCard.vue
+++ src/components/ProfileCard.vue
@@ -3,8 +3,8 @@
     <v-img :src="img" aspect-ratio="1" v-on:click="moveToProfile">
       <!-- <span style="background-color: #fefefe" class="h6 pa-1" justify="center">{{name}}</span> -->
       <v-row v-if="hover">
-        <v-card tile style="background-color: #fefefe" min-width="100%" align-end>
-          <v-card-title class="pa-2 h6 justify-center">{{ name }}</v-card-title>
+        <v-card tile style="background-color: #fefefe" class="to-bottom" min-width="100%" align-end>
+          <v-card-title class="pa-2 h6 justify-center fix">{{ name }}</v-card-title>
         </v-card>
       </v-row>
     </v-img>
@@ -53,5 +53,20 @@ export default {
 }
 .picop {
   opacity: 0.5;
+}
+.to-bottom {
+  position: absolute;
+  bottom: 0;
+  margin-left: auto;
+  margin-right: auto;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+.fix {
+    overflow-wrap: anywhere;
+    word-wrap: break-word;
+    word-break: normal;
+    hyphens: auto;
 }
 </style>
