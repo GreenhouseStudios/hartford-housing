09ff878b21c12ddd1330c6a2eb4e77a2d239264f
Thomas Lee <thomas.lee@uconn.edu>
2021-03-03 08:31:46 -0500

    rm youtube embed

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueFullPage from 'vue-fullpage.js'
-import VueYouTubeEmbed from 'vue-youtube-embed'
 import vuetify from './plugins/vuetify';
 import router from './router'
 import VueScrollmagic from 'vue-scrollmagic'
