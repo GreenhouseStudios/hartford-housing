93548a6e6b2dacbc767cf8147384c033fa8bcc99
Thomas Lee <thomas.lee@uconn.edu>
2021-08-02 15:07:28 -0400

    intro card mobile

--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -1,49 +1,75 @@
 <template>
   <div class="section nav-section" id="intro-section">
     <v-sheet height="100vh" width="100vw" color="green">
-      <v-container fluid fill-height pa-0>
-        <v-row justify="center"
-          ><h1
-            class="
-              d-none d-md-block
-              display-3
-              font-weight-bold
-              highlight-teal
-              text-center
-            "
-            id="intro-title"
-          >
-            Introduction
-          </h1>
-          <h1
-            class="
-              d-block d-md-none
-              display-1
-              font-weight-bold
-              highlight-teal
-              text-center
-            "
-            id="intro-title"
-          >
-            Introduction
-          </h1></v-row
+      <v-row justify="center"
+        ><h1
+          class="
+            d-none d-md-block
+            display-4
+            font-weight-bold
+            highlight-teal
+            text-center
+          "
+          id="intro-title"
         >
-        <v-row>
-          <v-col cols="3"></v-col>
-          <v-col cols="6">
-            <v-card class="pa-10">
-              <v-card-title></v-card-title>
-              <v-card-text>{{ lorem.generateParagraphs(3) }}</v-card-text>
-              <v-card-text>{{ lorem.generateParagraphs(4) }}</v-card-text>
-              <v-card-actions>
-                <a href="../assets/PDFs/sample.pdf" download class="font-weight-bold" style="text-decoration: none;">Read the Full Essay</a>
-              </v-card-actions>
-            </v-card>
-            <!-- <Card></Card> -->
-          </v-col>
-          <v-col cols="3"></v-col>
-        </v-row>
-      </v-container>
+          Introduction
+        </h1>
+        <h1
+          class="
+            d-block d-md-none
+            display-1
+            font-weight-bold
+            highlight-teal
+            text-center
+          "
+          id="intro-title"
+        >
+          Introduction
+        </h1></v-row
+      >
+      <v-row class="mt-10 d-none d-md-flex justify-center">
+          <v-card class="pa-10 ma-10" max-width="45%">
+            <v-card-title class="display-2 font-weight-bold"
+              >Sample Title</v-card-title
+            >
+            <v-card-text style="font-size: x-large; line-height: 1.5em">{{
+              lorem.generateParagraphs(3)
+            }}</v-card-text>
+            <v-card-actions>
+              <a
+                href="../assets/PDFs/sample.pdf"
+                download
+                class="font-weight-bold"
+                style="text-decoration: none"
+                >Read the Full Essay</a
+              >
+            </v-card-actions>
+          </v-card>
+        
+      </v-row>
+      
+      <v-row class="d-flex d-md-none">
+        <v-col cols="0" md="3"></v-col>
+        <v-col cols="12" md="6">
+          <v-card>
+            <v-card-title class="font-weight-bold">Sample Title</v-card-title>
+            <v-card-text style="font-size: small; line-height: 1.5em">{{
+              lorem.generateParagraphs(3)
+            }}</v-card-text>
+            <v-card-actions>
+              <a
+                href="../assets/PDFs/sample.pdf"
+                download
+                class="font-weight-bold"
+                style="text-decoration: none"
+                >Read the Full Essay</a
+              >
+            </v-card-actions>
+          </v-card>
+     
+        </v-col>
+        <v-col cols="" md="3"></v-col>
+      </v-row>
     </v-sheet>
   </div>
 </template>
